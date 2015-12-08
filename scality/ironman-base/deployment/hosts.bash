d08e2effe2dc202e9a2cf40c8cba7c222a6c446d
Marc Ségura <marc.segura@scality.com>
2015-12-08 02:58:21 +0100

    Adapt the hosts.bash to deal with the limitations of old docker versions (circleci)

--- scality/ironman-base/deployment/hosts.bash
+++ scality/ironman-base/deployment/hosts.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 # fix the hostnames
-for name in $(docker ps --format "{{.Names}}") ; do echo $name ; docker exec -u root $name /sbin/sysctl -w kernel.hostname=$name ; done
+# does not work in circleci
+#for name in $(docker ps --format "{{.Names}}") ; do echo $name ; docker exec -u root $name /sbin/sysctl -w kernel.hostname=$name ; done
 # fix /etc/hosts
 tempfile=/tmp/hosts$RANDOM
 for cid in $(docker ps -q) ; do 
-    	name=$(docker inspect ${cid} | grep Name | head -n 1 |  cut -d\/ -f 2 | cut -d\" -f 1);
+    	name=$(docker inspect ${cid} | grep Name | grep -v \"\" | head -n 1 |  cut -d\/ -f 2 | cut -d\" -f 1);
     	ip=$(docker inspect ${cid} | grep -v \"\" | grep \"IPAddress\" | cut -d\" -f 4 | head -n 1)
     	ipv6=$(docker inspect ${cid}| grep -v \"\" | grep \"GlobalIPv6Address\" | cut -d\" -f 4 | head -n 1)
     	if [ ! -z "${ip}" ]; then
